eac4d1cb027f251945d57908b60de94b66cdb6b6
Alexander Steinhauer <alexander.steinhauer@tum.de>
2024-02-13 02:00:37 +0100

    Support other platforms than macOS

--- Package.swift
+++ Package.swift
@@ -1,22 +1,18 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftUCI",
     platforms: [
-        .macOS(.v13)
+        .iOS(.v16), .macOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUCI",
             targets: ["SwiftUCI"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUCI",
             swiftSettings: [.enableUpcomingFeature("BareSlashRegexLiterals")]),
